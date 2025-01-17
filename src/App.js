73d5e51b0149578c0ead8d7454f409fb454ff43d
Alina Balakireva <abalakireva@aviasoft.ru>
2025-01-17 15:43:44 +0300

    added reactRouter

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route, Router } from "react-router";
 
 function App() {
   return (
-    <div className="App">
-      <h2>Roomify</h2>
-      <span>help</span>
+    <div>
+      
     </div>
   );
 }
