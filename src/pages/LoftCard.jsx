6485263e9b9495dcc45d512cf86494b0585c9494
Alina Balakireva <abalakireva@aviasoft.ru>
2025-02-10 17:08:38 +0300

    added LoftCard.jsx

---
+++ src/pages/LoftCard.jsx
@@ -0,0 +1,11 @@
+const LoftCard = ()=>{
+    return (
+        <div className="allLoftCard">
+            <div className="loftCardContainer">
+                <div>meow</div>
+            </div>
+        </div>
+    )
+}
+
+export default LoftCard;
