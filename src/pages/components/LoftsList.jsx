0c673fcd54d5f7a77cd472692690ca638c4468fe
Alina Balakireva <abalakireva@aviasoft.ru>
2025-02-10 16:36:59 +0300

    loftslift

--- src/pages/components/LoftsList.jsx
+++ src/pages/components/LoftsList.jsx
@@ -1,3 +1,4 @@
+import imageAmbra from "../../images/Lofts/loftsImage/imageAmbra.svg";
 import './styles/LoftsList.css';
 
 const LoftsList = ()=>{
@@ -6,7 +7,60 @@ const LoftsList = ()=>{
             <div className="loftsListContainer">
                 <div className="loftsListElements">
                     <div className="loftsListElement">
-                        mew
+                        <div className="loftsListElementImage">
+                            <img src={imageAmbra} />
+                        </div>
+                        <div className="loftsListElementText">
+                            <div className="loftsListElementName">Лофт "Амбра"</div>
+                            <div className="loftsListElementLocation">м. Таганская</div>
+                            <div className="loftsListElementPeople">20м, 12 чел.</div>
+                            <div className="loftsListElementBottomText">
+                                <div className="loftsListElementPrice">от 1500р/час</div>
+                                <div className="loftsListElementRating">Нет оценок</div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="loftsListElement">
+                        <div className="loftsListElementImage">
+                            <img src={imageAmbra} />
+                        </div>
+                        <div className="loftsListElementText">
+                            <div className="loftsListElementName">Лофт "Амбра"</div>
+                            <div className="loftsListElementLocation">м. Таганская</div>
+                            <div className="loftsListElementPeople">20м, 12 чел.</div>
+                            <div className="loftsListElementBottomText">
+                                <div className="loftsListElementPrice">от 1500р/час</div>
+                                <div className="loftsListElementRating">Нет оценок</div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="loftsListElement">
+                        <div className="loftsListElementImage">
+                            <img src={imageAmbra} />
+                        </div>
+                        <div className="loftsListElementText">
+                            <div className="loftsListElementName">Лофт "Амбра"</div>
+                            <div className="loftsListElementLocation">м. Таганская</div>
+                            <div className="loftsListElementPeople">20м, 12 чел.</div>
+                            <div className="loftsListElementBottomText">
+                                <div className="loftsListElementPrice">от 1500р/час</div>
+                                <div className="loftsListElementRating">Нет оценок</div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="loftsListElement">
+                        <div className="loftsListElementImage">
+                            <img src={imageAmbra} />
+                        </div>
+                        <div className="loftsListElementText">
+                            <div className="loftsListElementName">Лофт "Амбра"</div>
+                            <div className="loftsListElementLocation">м. Таганская</div>
+                            <div className="loftsListElementPeople">20м, 12 чел.</div>
+                            <div className="loftsListElementBottomText">
+                                <div className="loftsListElementPrice">от 1500р/час</div>
+                                <div className="loftsListElementRating">Нет оценок</div>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
