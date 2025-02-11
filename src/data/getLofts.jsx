923081d8290ebc4b8aa480213b63100431d0054e
Alina Balakireva <abalakireva@aviasoft.ru>
2025-02-11 13:25:33 +0300

    required getLofts.jsx to LoftsList.jsx, added method .map()

--- src/data/getLofts.jsx
+++ src/data/getLofts.jsx
@@ -2,6 +2,8 @@ export function getLofts(){
     return [
         {id: 1, name: "love", metro: "Бауманская", capacity: 40, priceWeekdays: 1900, priceWeekends: 3500, square: 100, type: 1},
         {id: 2, name: "light", metro: "Площадь Ильича", capacity: 25, priceWeekdays: 1300, priceWeekends: 3000, square: 45, type: 1},
+        {id: 3, name: "ambra", metro: "Фили", capacity: 15, priceWeekdays: 1500, priceWeekends: 3000, square: 20, type: 1},
+        {id: 4, name: "pupi", metro: "Жулебино", capacity: 44, priceWeekdays: 2000, priceWeekends: 5000, square: 150, type: 1},
     ]
 
 }
--- src/pages/components/LoftsList.jsx
+++ src/pages/components/LoftsList.jsx
@@ -1,70 +1,35 @@
 import { Link } from "react-router";
 import imageAmbra from "../../images/Lofts/loftsImage/imageAmbra.svg";
 import './styles/LoftsList.css';
+import { useState } from "react";
+import { getLofts } from "../../data/getLofts";
 
 const LoftsList = () => {
+    
+    const [loftsList, setLoftsList] = useState(getLofts());
+
     return (
         <div className="loftsListAll">
             <div className="loftsListContainer">
                 <div className="loftsListElements">
-                    <Link className="link-card" to='/card'>
-                        <div className="loftsListElement">
-                            <div className="loftsListElementImage">
-                                <img src={imageAmbra} />
-                            </div>
-                            <div className="loftsListElementText">
-                                <div className="loftsListElementName">Лофт "Амбра"</div>
-                                <div className="loftsListElementLocation">м. Таганская</div>
-                                <div className="loftsListElementPeople">20м, 12 чел.</div>
-                                <div className="loftsListElementBottomText">
-                                    <div className="loftsListElementPrice">от 1500р/час</div>
-                                    <div className="loftsListElementRating">Нет оценок</div>
+                    {loftsList.map(loft => (
+                        <Link className="link-card" to='/card'>
+                            <div className="loftsListElement">
+                                <div className="loftsListElementImage">
+                                    <img src={imageAmbra} />
+                                </div>
+                                <div className="loftsListElementText">
+                                    <div className="loftsListElementName">Лофт "{loft.name}"</div>
+                                    <div className="loftsListElementLocation">м. {loft.metro}</div>
+                                    <div className="loftsListElementPeople">{loft.square}м, {loft.capacity} чел.</div>
+                                    <div className="loftsListElementBottomText">
+                                        <div className="loftsListElementPrice">от {loft.priceWeekdays}р/час</div>
+                                        <div className="loftsListElementRating">Нет оценок</div>
+                                    </div>
                                 </div>
                             </div>
-                        </div>
-                    </Link>
-                    <div className="loftsListElement">
-                        <div className="loftsListElementImage">
-                            <img src={imageAmbra} />
-                        </div>
-                        <div className="loftsListElementText">
-                            <div className="loftsListElementName">Лофт "Амбра"</div>
-                            <div className="loftsListElementLocation">м. Таганская</div>
-                            <div className="loftsListElementPeople">20м, 12 чел.</div>
-                            <div className="loftsListElementBottomText">
-                                <div className="loftsListElementPrice">от 1500р/час</div>
-                                <div className="loftsListElementRating">Нет оценок</div>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="loftsListElement">
-                        <div className="loftsListElementImage">
-                            <img src={imageAmbra} />
-                        </div>
-                        <div className="loftsListElementText">
-                            <div className="loftsListElementName">Лофт "Амбра"</div>
-                            <div className="loftsListElementLocation">м. Таганская</div>
-                            <div className="loftsListElementPeople">20м, 12 чел.</div>
-                            <div className="loftsListElementBottomText">
-                                <div className="loftsListElementPrice">от 1500р/час</div>
-                                <div className="loftsListElementRating">Нет оценок</div>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="loftsListElement">
-                        <div className="loftsListElementImage">
-                            <img src={imageAmbra} />
-                        </div>
-                        <div className="loftsListElementText">
-                            <div className="loftsListElementName">Лофт "Амбра"</div>
-                            <div className="loftsListElementLocation">м. Таганская</div>
-                            <div className="loftsListElementPeople">20м, 12 чел.</div>
-                            <div className="loftsListElementBottomText">
-                                <div className="loftsListElementPrice">от 1500р/час</div>
-                                <div className="loftsListElementRating">Нет оценок</div>
-                            </div>
-                        </div>
-                    </div>
+                        </Link>
+                    ))}
                 </div>
             </div>
         </div>
