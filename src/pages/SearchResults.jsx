1372e65780b4478cb622734d27c2beac4c5524af
Alina Balakireva <abalakireva@aviasoft.ru>
2025-03-12 19:45:07 +0300

    запрос в searchResults (а вдруг это просто постман идиот,,,, но нет это мы)

--- src/pages/SearchResults.jsx
+++ src/pages/SearchResults.jsx
@@ -1,4 +1,32 @@
+import React, { useEffect, useState } from "react";
+
 const SearchResults = () => {
 
+    const [searchData, setSearchData] = useState([]);
+    const [err, setErr] = useState(null);
+
+
+    useEffect(()=>{
+        const fetchSearchData = async()=>{
+            try {
+                const response = await fetch('http://localhost:3002/search');
+                if (!response.ok) {
+                    throw new Error(`Ошибка HTTP! Статус: ${response.status}`);
+                }
+                const json = await response.json();
+                setSearchData(json)
+            } catch (err) {
+                setErr(err.message);
+                console.error("Ошибка при загрузке данных", err);
+            }
+        };
+        fetchSearchData();
+    }, []);
+    console.log(searchData)
+    return (
+        <div>
+            mew
+        </div>
+    )
 };
 export default SearchResults;
